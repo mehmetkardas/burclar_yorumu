feb2390494d430d11c3c3d7a7dfb36d1553b6bcc
“mehmetkardas” <mehmetgskmp31@gmail.com>
2025-07-08 15:10:32 +0300

    first commit

---
+++ lib/widgets/burc_item.dart
@@ -0,0 +1,32 @@
+import 'package:burclar_yorumu/model/burc.dart';
+import 'package:flutter/material.dart';
+
+class BurcItem extends StatelessWidget {
+  final Burc gelenBurc;
+  const BurcItem({super.key, required this.gelenBurc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(4),
+      child: Card(
+        elevation: 4,
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: Image.asset("images/${gelenBurc.burcKucukResim}"),
+            title: Text(
+              gelenBurc.burcAdi,
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            subtitle: Text(
+              gelenBurc.burcTarih,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            trailing: Icon(Icons.arrow_forward_ios, color: Colors.pink),
+          ),
+        ),
+      ),
+    );
+  }
+}
