9274b4607ddab775d46ce0ae0d3d32bf0b416525
“mehmetkardas” <mehmetgskmp31@gmail.com>
2025-07-09 10:08:22 +0300

    sondetaylar

--- lib/pages/burc_detay.dart
+++ lib/pages/burc_detay.dart
@@ -2,9 +2,35 @@ import 'package:burclar_yorumu/model/burc.dart';
 import 'package:flutter/material.dart';
 import 'package:palette_generator/palette_generator.dart';
 
-class BurcDetay extends StatelessWidget {
+class BurcDetay extends StatefulWidget {
   final Burc secilenBurc;
   const BurcDetay({super.key, required this.secilenBurc});
+
+  @override
+  State<BurcDetay> createState() => _BurcDetayState();
+}
+
+class _BurcDetayState extends State<BurcDetay> {
+  //Burc detay sayfasında yukarı kaydırınca büyük resmin rengini alıp appbara veririz.
+  Color appBarRengi = Colors.pink;
+  late PaletteGenerator _generator;
+
+  @override
+  void initState() {
+    appBarRenginiBul();
+    super.initState();
+  }
+
+  void appBarRenginiBul() async {
+    _generator = await PaletteGenerator.fromImageProvider(
+      AssetImage("images/${widget.secilenBurc.burcBuyukResim}"),
+    );
+
+    setState(() {
+      appBarRengi = _generator.vibrantColor!.color;
+    });
+    print(appBarRengi);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +38,14 @@ class BurcDetay extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            backgroundColor: appBarRengi,
             expandedHeight: 250,
             pinned: true,
             flexibleSpace: FlexibleSpaceBar(
               centerTitle: true,
-              title: Text("${secilenBurc.burcAdi} Burcu ve Özellikleri"),
+              title: Text("${widget.secilenBurc.burcAdi} Burcu ve Özellikleri"),
               background: Image.asset(
-                "images/${secilenBurc.burcBuyukResim}",
+                "images/${widget.secilenBurc.burcBuyukResim}",
                 fit: BoxFit.cover,
               ),
             ),
@@ -30,7 +57,7 @@ class BurcDetay extends StatelessWidget {
               padding: EdgeInsets.all(8),
               child: SingleChildScrollView(
                 child: Text(
-                  secilenBurc.burcDetay,
+                  widget.secilenBurc.burcDetay,
                   style: Theme.of(context).textTheme.headlineSmall,
                 ),
               ),
